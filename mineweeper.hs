848560b9fa4ea9efa4e4a72ff950d3ba86a3eb9c
Sondre N <sondrenordmoen@hotmail.com>
2022-02-02 16:16:28 +0100

    ha

--- mineweeper.hs
+++ mineweeper.hs
@@ -20,8 +20,23 @@ header = 2
 margin_left :: Int
 margin_left = 1
 
--- terminal indexing starts at 0, 0, upper left corner
+tile_size :: Int
+tile_size = 1
 
+divisor_size_vertical :: Int
+divisor_size_vertical = 0
+
+divisor_size_horizontal :: Int
+divisor_size_horizontal = 1
+
+cell_size_vertical :: Int
+cell_size_vertical = 1
+
+cell_size_horizontal :: Int
+cell_size_horizontal = 1
+
+
+-- terminal indexing starts at 0, 0, upper left corner
 
 
 -- ####################################### Pretty Printing
@@ -46,23 +61,45 @@ standardAttribute = defAttr `withForeColor` white
 - Left for revealed state
 - Right for state of the game beneath the surface
 -}
-
 newGame :: Int -> Int -> Game
-newGame r c = (newBoard r c, newBoard r c)
+newGame r c = (newShown r c, newHidden r c)
 
 
-newBoard :: Int -> Int -> [[Int]]
-newBoard rows columns = take rows (repeat (take columns (repeat 0)))
+-- 
+newShown :: Int -> Int -> [[Int]]
+newShown rows columns = take rows (repeat (take columns (repeat (-3))))
+
+newHidden :: Int -> Int -> [[Int]]
+newHidden _ _ = calculateHidden boardTest
+--newHidden rows columns = take rows (repeat (take columns (repeat (0))))
+
+calculateHidden :: [[Int]] -> [[Int]]
+calculateHidden board = [[if c == (-1) then c else countBombs board (x, y) | (c, x) <- (zip r [0..])] | (r, y) <- (zip board [0..])]
+
+countBombs :: [[Int]] -> Pos -> Int
+countBombs board (0, 0) =   sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 r] | r <- (take 2 board)]
+countBombs board (0, y)
+    | y == length board =   sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 r] | r <- (drop ((length board)-2) board)]
+    | otherwise =           sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 r] | r <- (take 3 (drop (y-1) board))]
+countBombs board (x, 0)
+    | x == length (head board) =    sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 (drop (x-1) r)] | r <- (take 2 board)]
+    | otherwise =                   sum $ concat [[if c == (-1) then 1 else 0 | c <- take 3 (drop (x-1) r)] | r <- (take 2 board)]
+countBombs board (x, y) 
+    | x == length (head board) && y == length board = sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 (drop (x-1) r)] | r <- (drop ((length board)-2) board)]
+    | x == length (head board) =    sum $ concat [[if c == (-1) then 1 else 0 | c <- take 2 (drop (x-1) r)] | r <- (take 3 (drop (y-1) board))]
+    | y == length board =           sum $ concat [[if c == (-1) then 1 else 0 | c <- take 3 (drop (x-1) r)] | r <- (drop ((length board)-2) board)]
+    | otherwise =                   sum $ concat [[if c == (-1) then 1 else 0 | c <- take 3 (drop (x-1) r)] | r <- (take 3 (drop (y-1) board))]
 
 
 -- (-1) represents bomb, have to calculate actual values
+-- (-3) are exclusive to the rendered board and represents an un-turned value
 boardTest :: [[Int]]
-boardTest = [[0, 0, 0,  0, 0,  0],
-             [0, 0, 0,  0, 0, -1],
-             [0, 0, 0, -1, 0,  0],
-             [0, 0, 0,  0, 0,  0],
-             [0, 0, 0,  0, 0,  0],
-             [1, 2, 3,  4, 5,  6]]
+boardTest = [[-3,-1, 0,  0, 0,  0],
+             [-3, 0, 0,  0, 0, -1],
+             [-3, 0, 0, -1, 0,  0],
+             [-1, 0, 0,  0, 0,  0],
+             [ 0, 0, 0,  0, 0,  0],
+             [ 1, 2, 3,  4, 5,  6]]
 
 
 
@@ -87,7 +124,8 @@ formatInt :: Int -> String
 formatInt x
     | x == -1 = "b"
     | x == -2 = "f"
-    | x ==  0 = '\2588':""
+    | x == -3 = '\2658':""
+    | x ==  0 = ' ':""
     | otherwise = show x 
 
 
@@ -111,21 +149,38 @@ renderBoard (shown, hidden) vty text = do
 
 
 
+
 -- TODO check if click-ed spot is a part of the board
 -- TODO Make board logic
 
+getValue :: [[Int]] -> Pos -> Int
+getValue board (x, y) = last $ take (x+1) (last $ take (y+1) board) 
+
 
 -- Translates terminal-coordinates to game-board coordinates (if possible)
-translateCoordinate :: Pos -> Maybe Pos
-translateCoordinate (x, y)
-    | mod (x - 1) 2 == 0 && y >= header = Just (div (x - margin_left) 2 , div (y - header) 1)
+translateCoordinate :: Pos -> Game -> Maybe Pos
+translateCoordinate (x, y) (shown, hidden)
+    | x >= margin_left + (divisor_size_horizontal + cell_size_horizontal) * (length $ head shown) = Nothing
+    | y >= header      + (divisor_size_vertical   + cell_size_vertical)   * (length shown)        = Nothing
+    | mod (x - 1) 2 == 0 && y >= header = Just (div (x - margin_left) (divisor_size_horizontal + cell_size_horizontal) , div (y - header) (divisor_size_vertical + cell_size_vertical))
     | otherwise = Nothing
 
 
+checkTile :: Game -> Pos -> Game
+checkTile (shown, hidden) (x, y)
+    | getValue shown (x, y) == -3 = (insertIntoGrid shown (x, y) (getValue hidden (x, y)), hidden) -- ((take (x)) . (last $ take (y+1))) shown ++ getValue hidden (x, y) ++ 
+    | otherwise = (shown, hidden)
 
+
+-- Inserts a number into the grid by taking all elements until the element, adding the element, and then taking all elements after the element
+insertIntoGrid :: [[Int]] -> Pos -> Int -> [[Int]]
+insertIntoGrid board (x, y) val = take (y) board ++ [take x (last $ take (y+1) board) ++ [val] ++ drop (x+1) (last $ take (y + 1) board)] ++ drop (y+1) board
 
 revealBoard :: Game -> Pos -> Game
-revealBoard (shown, hidden) (x, y) = undefined
+revealBoard (shown, hidden) (x, y) =
+    case (translateCoordinate (x, y) (shown, hidden)) of 
+        Nothing -> (shown, hidden)
+        Just (x1, y1) -> checkTile (shown, hidden) (x1, y1)
 
 
 
@@ -139,11 +194,11 @@ mainLoop (EvKey key m) vty (shown, hidden) (x, y) = do
         renderBoard (shown, hidden) vty (show key)
         event <- nextEvent vty
         mainLoop event vty (shown, hidden) (x, y)
-
 mainLoop (EvMouseDown x_mouse y_mouse button modifiers) vty (shown, hidden) (x, y) = do
-    renderBoard (shown, hidden) vty (unwords $ (map show [x_mouse, y_mouse]) ++ [show button])
+    let newGame = revealBoard (shown, hidden) (x_mouse, y_mouse)
+    renderBoard newGame vty (unwords $ (map show [x_mouse, y_mouse]) ++ [show button])
     event <- nextEvent vty
-    mainLoop event vty (shown, hidden) (x, y)
+    mainLoop event vty newGame (x, y)
 
 -- mainLoop (EvMouseUp x_mouse y_mouse button modifiers) vty (shown, hidden) (x, y)
 mainLoop event vty (shown, hidden) (x, y) = do -- Default event when not a key or a mousebuuton : do nothing
@@ -202,7 +257,7 @@ main  = do
             putStrLn "Row and columns need to be numbers"
             exitWith (ExitFailure 1)
     else do
-            let board = (boardTest, boardTest)  -- (newBoard 6 6,boardTest) --makeBoardText rows columns
+            let board = (newShown 6 6, boardTest)  -- (newBoard 6 6,boardTest) --makeBoardText rows columns
             print board
 
             cfg <- standardIOConfig
@@ -212,7 +267,9 @@ main  = do
             --putStrLn $ show output
             setMode output Mouse True
             -- TODO -initiate board here-
-    
+            
+            renderBoard board vty ("Top")
+
             event <- nextEvent vty
             mainLoop event vty board (3, 3)
             shutdown vty
@@ -246,3 +303,4 @@ main  = do
         pic = picForImage temp
     update vty pic
     e <- nextEvent vty-}
+
